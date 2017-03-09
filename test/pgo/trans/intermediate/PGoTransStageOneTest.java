d571acc8e180966c060d6f4dd963c17d3f43993a
Bob Yang <bobyangyf@gmail.com>
2017-03-09 12:17:58 -0800

    Add more variable tests for each pluscal sample

--- test/pgo/trans/intermediate/PGoTransStageOneTest.java
+++ test/pgo/trans/intermediate/PGoTransStageOneTest.java
@@ -2,6 +2,7 @@ package pgo.trans.intermediate;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -44,8 +45,17 @@ public class PGoTransStageOneTest extends PcalASTBase {
 	public void testAlgName() throws PGoTransException {
 		for (String alg : pcalAlgs) {
 			PGoTransStageOne p = new PGoTransStageOne(ast.get(alg));
-			assertEquals(p.getAlgName(), alg);
+			assertEquals(alg, p.getAlgName());
 		}
+	}
+
+	// assert function for a pgovariable generated from initial pass
+	protected void assertPGoVariable(PGoTransStageOne p, String varName, boolean isSimpleAssignInit, String initBlock) {
+		PGoVariable v = p.getGlobal(varName);
+		assertNotNull(v);
+		assertEquals(varName, v.getName());
+		assertEquals(isSimpleAssignInit, v.getIsSimpleAssignInit());
+		assertEquals(initBlock, v.getPcalInitBlock().toString());
 	}
 
 	@Test
@@ -53,24 +63,75 @@ public class PGoTransStageOneTest extends PcalASTBase {
 		PGoTransStageOne p = new PGoTransStageOne(ast.get("Euclid"));
 		Collection<PGoVariable> cv = p.getGlobals();
 
-		assertEquals(cv.size(), 3);
+		assertEquals(3, cv.size());
 
-		PGoVariable[] var = cv.toArray(new PGoVariable[cv.size()]);
+		assertPGoVariable(p, "u", true, "<< \"24\" >>");
+		assertPGoVariable(p, "v", false, "<< \"1\", \"..\", \"N\" >>");
+		assertPGoVariable(p, "v_init", true, "<< \"v\" >>");
+	}
 
-		PGoVariable u = var[0];
-		assertEquals(u.getName(), "u");
-		assertTrue(u.getIsSimpleAssignInit());
-		assertEquals(u.getPcalInitBlock().toString(), "<< \"24\" >>");
+	@Test
+	public void testVarFromQueensPluscal() throws PGoTransException {
+		PGoTransStageOne p = new PGoTransStageOne(ast.get("QueensPluscal"));
+		Collection<PGoVariable> cv = p.getGlobals();
 
-		PGoVariable v = var[1];
-		assertEquals(v.getName(), "v");
-		assertFalse(v.getIsSimpleAssignInit());
-		assertEquals(v.getPcalInitBlock().toString(), "<< \"1\", \"..\", \"N\" >>");
+		assertEquals(cv.size(), 2);
 
-		PGoVariable v_init = var[2];
-		assertEquals(v_init.getName(), "v_init");
-		assertTrue(v_init.getIsSimpleAssignInit());
-		assertEquals(v_init.getPcalInitBlock().toString(), "<< \"v\" >>");
+		assertPGoVariable(p, "todo", true, "<< \"{\", \"<<\", \">>\", \"}\" >>");
+		assertPGoVariable(p, "sols", true, "<< \"{\", \"}\" >>");
+	}
+
+	@Test
+	public void testVarFromQueensPluscalProcedure() throws PGoTransException {
+		PGoTransStageOne p = new PGoTransStageOne(ast.get("QueensPluscalProcedure"));
+		Collection<PGoVariable> cv = p.getGlobals();
+
+		assertEquals(3, cv.size());
+
+		assertPGoVariable(p, "todo", true, "<< \"{\", \"<<\", \">>\", \"}\" >>");
+		assertPGoVariable(p, "sols", true, "<< \"{\", \"}\" >>");
+		assertPGoVariable(p, "rVal", true, "<< \"defaultInitValue\" >>");
+	}
+
+	@Test
+	public void testVarFromSum() throws PGoTransException {
+		PGoTransStageOne p = new PGoTransStageOne(ast.get("Sum"));
+		Collection<PGoVariable> cv = p.getGlobals();
+
+		assertEquals(cv.size(), 1);
+
+		assertPGoVariable(p, "network", true,
+				"<< \"[\", \"i\", \"\\\\in\", \"1\", \"..\", \"N\", \"+\", \"1\","
+						+ " \"|->\", \"<<\", \">>\", \"]\" >>");
+	}
+
+	@Test
+	public void testVarFromTwoPhaseCommit() throws PGoTransException {
+		PGoTransStageOne p = new PGoTransStageOne(ast.get("TwoPhaseCommit"));
+		Collection<PGoVariable> cv = p.getGlobals();
+
+		assertEquals(2, cv.size());
+
+		assertPGoVariable(p, "managers", true,"<< \"{\", \"\\\"\", \"bob\", \"\\\"\","
+				+ " \",\", \"\\\"\", \"chuck\", \"\\\"\", \",\", \"\\\"\", \"dave\", "
+				+ "\"\\\"\", \",\", \"\\\"\", \"everett\", \"\\\"\", \",\", \"\\\"\","
+				+ " \"fred\", \"\\\"\", \"}\" >>");
+				
+		assertPGoVariable(p, "restaurant_stage", true,
+				"<< \"[\", \"mgr\", \"\\\\in\", \"managers\", \"|->\", \"\\\"\", \"start\", \"\\\"\", \"]\" >>");
+	}
+
+	@Test
+	public void testVarFromFastMutex() throws PGoTransException {
+		PGoTransStageOne p = new PGoTransStageOne(ast.get("FastMutex"));
+		Collection<PGoVariable> cv = p.getGlobals();
+
+		assertEquals(3, cv.size());
+
+		assertPGoVariable(p, "x", true, "<< \"defaultInitValue\" >>");
+		assertPGoVariable(p, "y", true, "<< \"0\" >>");
+		assertPGoVariable(p, "b", true,
+				"<< \"[\", \"i\", \"\\\\in\", \"1\", \"..\", \"N\", \"|->\", \"FALSE\", \"]\" >>");
 	}
 
 }
