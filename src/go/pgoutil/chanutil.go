aa537aed0ab50cf4ad4f72546376acbe771e63ef
Brandon Zhang <brandonzhang53@gmail.com>
2017-06-20 17:55:36 -0700

    Add channel initialization with elements to go library

---
+++ src/go/pgoutil/chanutil.go
@@ -0,0 +1,10 @@
+package pgoutil
+
+// Return a channel initialized with elements.
+func NewChan(elts ...interface{}) chan interface{} {
+	ret := make(chan interface{}, len(elts))
+	for _, elt := range elts {
+		ret <- elt
+	}
+	return ret
+}
