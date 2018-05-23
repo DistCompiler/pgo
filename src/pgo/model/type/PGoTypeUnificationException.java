289f9f4e9155b6a6f316841e08cc685f6b26d5fd
Do Nhat Minh <mrordinaire@gmail.com>
2018-05-23 13:12:43 -0700

    removed unused exceptions

--- src/pgo/model/type/PGoTypeUnificationException.java
+++
@@ -1,9 +0,0 @@
-package pgo.model.type;
-
-public class PGoTypeUnificationException extends PGoTypeException {
-	private static final String PREFIX = "Type unification exception";
-
-	public PGoTypeUnificationException(PGoType a, PGoType b) {
-		super(PREFIX, "Unable to unify " + a.toTypeName() + " and " + b.toTypeName());
-	}
-}
