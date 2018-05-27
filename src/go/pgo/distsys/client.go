debb3c7d2b82010fb9bea89ea2b2f69618bc5d61
Renato Costa <renatomc@cs.ubc.ca>
2018-05-26 20:04:06 -0700

    Better documentation for public functions.

--- src/go/pgo/distsys/client.go
+++ src/go/pgo/distsys/client.go
@@ -1,15 +1,24 @@
 package distsys
 
+// stateHandler defines the interface of a state handler. Apart from the definitions
+// of localStateHandler, which manipulate state located within the running node's
+// local store, there is also the possibility that state lives in another node
+// in the system, and a similar interface is required
 type stateHandler interface {
 	GetState() (VarReferences, error)
 	ReleaseState(VarReferences) error
 }
 
+// remoteHandler implements stateHandler for the case where state lives in a different
+// node in the network
 type remoteHandler struct {
 	group       *VarReq
 	stateServer *StateServer
 }
 
+// GetState retrieves state that lives in another node in the system. The ownership table
+// needs to be locked before this function is called since the variable may have
+// moved.
 func (remote remoteHandler) GetState() (VarReferences, error) {
 	conn := remote.stateServer.connections.GetConnection(remote.group.Peer)
 	refs := VarReferences(map[string]*Reference{})
@@ -21,6 +30,8 @@ func (remote remoteHandler) GetState() (VarReferences, error) {
 	return refs, nil
 }
 
+// ReleaseState releases state that lives in another system. This function needs to be
+// called once a counterpart GetState call has succeeded.
 func (remote remoteHandler) ReleaseState(refs VarReferences) error {
 	var ok bool
 	conn := remote.stateServer.connections.GetConnection(remote.group.Peer)
@@ -32,6 +43,9 @@ func (remote remoteHandler) ReleaseState(refs VarReferences) error {
 	return nil
 }
 
+// stateBuilder returns a stateHandler that either manipulates state in the running node's
+// local store, or sends messages to the node that currently owns the state contained
+// in the VarReq struct.
 func stateBuilder(group *VarReq, ss *StateServer) stateHandler {
 	if group.Peer == ss.self {
 		return localStateHandler{group, ss.store}
@@ -40,6 +54,9 @@ func stateBuilder(group *VarReq, ss *StateServer) stateHandler {
 	return remoteHandler{group, ss}
 }
 
+// Acquire receives a BorrowSpec and returns a populated VarReferences struct containing
+// the values of the variables in the spec given, with the requested permissions (i.e.,
+// exclusive or non-exclusive (read-only) access).
 func (ss *StateServer) Acquire(spec *BorrowSpec) (VarReferences, error) {
 	op := GlobalStateOperation{
 		spec:      spec,
@@ -64,6 +81,9 @@ func (ss *StateServer) Acquire(spec *BorrowSpec) (VarReferences, error) {
 	return allRefs, nil
 }
 
+// Release receives a set of references to variables, potentially manipulated by
+// the application (after a call to Acquire), and releases any locks that were
+// held, either locally or remotely on a different node in the system.
 func (ss *StateServer) Release(refs VarReferences) error {
 	op := GlobalStateOperation{
 		spec:      refs.ToBorrowSpec(),
