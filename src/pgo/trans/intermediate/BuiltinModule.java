c7bc6e25c79cded8d2f06934c965a8db22d6405e
Renato Costa <renatomc@cs.ubc.ca>
2018-06-19 11:44:29 -0700

    Support operator symbols that have the same semantics.

--- src/pgo/trans/intermediate/BuiltinModule.java
+++ src/pgo/trans/intermediate/BuiltinModule.java
@@ -1,30 +1,37 @@
 package pgo.trans.intermediate;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import pgo.scope.ChainMap;
 
 public class BuiltinModule {
-	
+
 	Map<String, OperatorAccessor> operators;
-	
+
 	public BuiltinModule() {
 		this.operators = new HashMap<>();
 	}
-	
+
 	public BuiltinModule(BuiltinModule exts) {
 		this.operators = new ChainMap<>(exts.operators);
 	}
-	
+
 	public void addOperator(String name, OperatorAccessor op) {
 		operators.put(name, op);
 	}
-	
+
+	public void addOperators(List<String> names, OperatorAccessor op) {
+		for (String name : names) {
+			addOperator(name, op);
+		}
+	}
+
 	public Map<String, OperatorAccessor> getOperators(){
 		return operators;
 	}
-	
+
 	public void addDefinitionsToScope(TLAScopeBuilder scope) {
 		for(Map.Entry<String, OperatorAccessor> op : operators.entrySet()) {
 			scope.defineGlobal(op.getKey(), op.getValue().getUID());
