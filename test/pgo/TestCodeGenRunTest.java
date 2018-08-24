657d9c5a84394d1d136f3ca4e112962fa5a08f82
Renato Costa <renatomc@cs.ubc.ca>
2018-08-23 18:40:39 -0700

    Pass constants explicitly in integration tests.

--- test/pgo/TestCodeGenRunTest.java
+++ test/pgo/TestCodeGenRunTest.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import static pgo.IntegrationTestingUtils.testCompileFile;
 import static pgo.IntegrationTestingUtils.testRunGoCode;
@@ -16,10 +17,12 @@ import static pgo.IntegrationTestingUtils.testRunGoCode;
 @RunWith(Parameterized.class)
 public class TestCodeGenRunTest {
 	private String fileName;
+	private Map<String, String> constants;
 	private List<String> expected;
 
-	public TestCodeGenRunTest(String fileName, List<String> expected) {
+	public TestCodeGenRunTest(String fileName, Map<String, String> constants, List<String> expected) {
 		this.fileName = fileName;
+		this.constants = constants;
 		this.expected = expected;
 	}
 
@@ -28,22 +31,25 @@ public class TestCodeGenRunTest {
 		return Arrays.asList(new Object[][] {
 				{
 						"SimpleEither.tla",
+						Collections.emptyMap(),
 						Arrays.asList("[1 30]", "[1 30]", "[1 30]"),
 				},
 				{
 						"EitherBothBranches.tla",
+						Collections.emptyMap(),
 						Arrays.asList("[10 20]", "[10 20]"),
 				},
 				{
 						"EitherRepeatedExec.tla",
+						Collections.emptyMap(),
 						Collections.singletonList("3"),
-				},
+				}
 		});
 	}
 
 	@Test
 	public void test() throws IOException {
-		testCompileFile(Paths.get("test", "integration", fileName), Collections.emptyMap(),
+		testCompileFile(Paths.get("test", "integration", fileName), constants,
 				compiledFilePath -> testRunGoCode(compiledFilePath, expected));
 	}
 }
