7545d44001c83e8f68b813026f427eaefab64cc1
Do Nhat Minh <mrordinaire@gmail.com>
2018-08-29 16:52:25 -0700

    fixed execution instructions

--- pgo.sh
+++ pgo.sh
@@ -3,4 +3,4 @@
 # Runs PGo from the compiled class files, passing all command
 # line argument directly to main().
 
-java -ea -cp ./lib/plume.jar:./lib/tlatools.jar:./lib/json.jar:./bin/ pgo.PGoMain $*
+java -ea -cp ./lib/plume.jar:./lib/json.jar:./bin/ pgo.PGoMain $*
