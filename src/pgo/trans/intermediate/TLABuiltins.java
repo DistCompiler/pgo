cd91540f3b69445fb01cd09fdea91a1f64203824
Renato Costa <renatomc@cs.ubc.ca>
2019-04-20 19:44:23 -0700

    Recognize IsFiniteSet operator.

--- src/pgo/trans/intermediate/TLABuiltins.java
+++ src/pgo/trans/intermediate/TLABuiltins.java
@@ -717,6 +717,15 @@ public class TLABuiltins {
 					return new GoCall(new GoVariableName("len"), Collections.singletonList(arguments.get(0)));
 				}
 		));
+		FiniteSets.addOperator("IsFiniteSet", new TypelessBuiltinOperator(
+				1,
+				(origin, args, solver, generator) -> {
+					throw new TODO();
+				},
+				(builder, origin, registry, arguments, typeMap) -> {
+					throw new TODO();
+				}
+		));
 
 	}
 
