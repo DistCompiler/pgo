25a28aa806b17f76ffba8eeeeec3227167ec7331
Do Nhat Minh <minhnhdo@gmail.com>
2019-04-24 02:22:59 -0700

    workers with input and output

--- examples/mpcal/eval/main.go
+++ examples/mpcal/eval/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"time"
 )
 
 const (
@@ -13,8 +16,15 @@ const (
 	ETCD_CLIENT
 )
 
+var (
+	firstTimeStamp time.Time
+	initChan       = make(chan bool, 0)
+	startChan      = make(chan bool, 0)
+	doneChan       = make(chan bool, 0)
+)
+
 func usage(programName string) {
-	fmt.Fprintln(os.Stderr, "Usage:", programName, "<dummy|doozer|etcd> <address>")
+	fmt.Fprintln(os.Stderr, "Usage:", programName, "<nworker> <dummy|doozer|etcd> <address>")
 	os.Exit(1)
 }
 
@@ -40,14 +50,14 @@ func createClient(which int, address string) Client {
 		client, err := DialDoozer(address, "")
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error connecting to doozer client:", err)
-			os.Exit(2)
+			os.Exit(1)
 		}
 		return client
 	case ETCD_CLIENT:
 		client, err := NewEtcdClient(address)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error connecting to etcd client:", err)
-			os.Exit(2)
+			os.Exit(1)
 		}
 		return client
 	default:
@@ -64,37 +74,77 @@ func readToken(buffer []byte) int {
 	return len(buffer)
 }
 
-func main() {
-	if len(os.Args) != 3 {
-		usage(os.Args[0])
+func issueGet(self int, client Client, key string) (time.Time, time.Duration) {
+	before := time.Now()
+	_, err := client.Get(key)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "Unable to issue get for worker", self)
+		panic(err)
 	}
+	after := time.Now()
+
+	return after, after.Sub(before)
+}
+
+func issuePut(self int, client Client, key string, value string) (time.Time, time.Duration) {
+	before := time.Now()
+	err := client.Put(key, value)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, "Unable to issue put for worker", self)
+		panic(err)
+	}
+	after := time.Now()
+
+	return after, after.Sub(before)
+}
+
+func outputToLog(output *bufio.Writer, opType byte, offset time.Duration, latency time.Duration) {
+	fmt.Fprintf(output, "%c %d %d", opType, offset, latency)
+}
+
+func worker(self int, which int, address string) {
 	// set up connection to implementation
 	// example: client := createClient(1, "doozer:?ca=127.0.0.1:8046")
-	which := parseClient(os.Args[1])
-	if which < 0 {
-		usage(os.Args[0])
-	}
-	client := createClient(which, os.Args[2])
+	client := createClient(which, address)
 	defer client.Close()
+
+	output, err := os.Create(fmt.Sprintf("output/worker-%d.out", self))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Unable to open output for worker", self)
+		os.Exit(1)
+	}
+	writer := bufio.NewWriter(output)
+	defer output.Close()
+
+	input, err := os.Open(fmt.Sprintf("input/worker-%d.inp", self))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Unable to open input for worker", self)
+		os.Exit(1)
+	}
+	defer input.Close()
+
+	initChan <- true
+	<-startChan
+
 	// bench
 	buffer := make([]byte, BUFFER_SIZE)
 	current := 0
 	end := 0
 	readMore := false
 	lastTokenLength := -1
-	stdin := os.Stdin
+
 	for {
 		switch {
 		case end <= current:
 			// we're completely out of data in the buffer
-			n, err := stdin.Read(buffer)
+			n, err := input.Read(buffer)
 			if err == io.EOF {
 				// we're done!
-				goto exit
+				goto workerDone
 			}
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error reading from stdin:", err)
-				os.Exit(3)
+				fmt.Fprintln(os.Stderr, "Error reading from input:", err, "for worker", self)
+				os.Exit(1)
 			}
 			// we got more data
 			current = 0
@@ -107,13 +157,13 @@ func main() {
 			// copy the remaining data to the start of the buffer
 			n := copy(buffer, buffer[current:end])
 			if n != end-current {
-				fmt.Fprintf(os.Stderr, "Error copying data within buffer: %d bytes copied but %d bytes needed copying\n", n, end-current)
-				os.Exit(4)
+				fmt.Fprintf(os.Stderr, "Error copying data within buffer: %d bytes copied but %d bytes needed copying for worker %d\n", n, end-current, self)
+				os.Exit(1)
 			}
-			n, err := stdin.Read(buffer[end-current:])
+			n, err := input.Read(buffer[end-current:])
 			if err != io.EOF && err != nil {
-				fmt.Fprintln(os.Stderr, "Error reading from stdin:", err)
-				os.Exit(5)
+				fmt.Fprintln(os.Stderr, "Error reading from input:", err, "for worker", self)
+				os.Exit(1)
 			}
 			// we (maybe) got more data
 			end = end - current + n
@@ -142,11 +192,14 @@ func main() {
 				// we've retried and there's no more data
 				key := string(buffer[i : i+tokenLength])
 				if tokenLength < end-i && buffer[i+tokenLength] != '\n' {
-					fmt.Fprintln(os.Stderr, "Get command must be followed by new line; found", string(buffer[current:end]))
-					os.Exit(6)
+					fmt.Fprintln(os.Stderr, "Get command must be followed by new line; found", string(buffer[current:end]), "for worker", self)
+					os.Exit(1)
 				}
+
 				// time to issue the get!
-				client.Get(key)
+				timestamp, latency := issueGet(self, client, key)
+				outputToLog(writer, 'g', timestamp.Sub(firstTimeStamp), latency)
+
 				lastTokenLength = -1
 				readMore = false
 				current = i + tokenLength + 1
@@ -168,14 +221,14 @@ func main() {
 					continue
 				}
 				if tokenLength == end-i {
-					fmt.Fprintln(os.Stderr, "Incomplete command:", string(buffer[current:end]))
-					os.Exit(7)
+					fmt.Fprintln(os.Stderr, "Incomplete command:", string(buffer[current:end]), "for worker", self)
+					os.Exit(1)
 				}
 				// condition here: tokenlength < end-token
 				key := string(buffer[i : i+tokenLength])
 				if buffer[i+tokenLength] != ' ' {
-					fmt.Fprintln(os.Stderr, "Ill-formed command:", string(buffer[current:end]))
-					os.Exit(8)
+					fmt.Fprintln(os.Stderr, "Ill-formed command:", string(buffer[current:end]), "for worker", self)
+					os.Exit(1)
 				}
 				// read the value
 				i += tokenLength + 1
@@ -192,22 +245,82 @@ func main() {
 				// condition here: tokenLength < end-i || lastTokenLength == tokenLength
 				if tokenLength < end-i && buffer[i+tokenLength] != '\n' {
 					fmt.Println(string(buffer[i+tokenLength:i+tokenLength+1]), buffer[i+tokenLength] == '\n')
-					fmt.Fprintln(os.Stderr, "Put command must be followed by new line; found", string(buffer[current:end]))
-					os.Exit(9)
+					fmt.Fprintln(os.Stderr, "Put command must be followed by new line; found", string(buffer[current:end]), "for worker", self)
+					os.Exit(1)
 				}
 				// we've (possibly) retried and there's no more data
 				value := string(buffer[i : i+tokenLength])
+
 				// time to issue the put!
-				client.Put(key, value)
+				timestamp, latency := issuePut(self, client, key, value)
+				outputToLog(writer, 'p', timestamp.Sub(firstTimeStamp), latency)
+
 				lastTokenLength = -1
 				readMore = false
 				current = i + tokenLength + 1
 			default:
-				fmt.Fprintln(os.Stderr, "Unknown command:", string(buffer[current:current+3]))
-				os.Exit(10)
+				fmt.Fprintln(os.Stderr, "Unknown command:", string(buffer[current:current+3]), "for worker", self)
+				os.Exit(1)
 			}
 		}
 	}
-exit:
+workerDone:
+	doneChan <- true
+}
+
+func main() {
+	if len(os.Args) != 4 {
+		usage(os.Args[0])
+	}
+	// parse number of workers
+	n, err := strconv.ParseInt(os.Args[1], 10, 32)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Unable to parse number of worker; found:", os.Args[1])
+		os.Exit(2)
+	}
+	nworker := int(n)
+	// parse client
+	which := parseClient(os.Args[2])
+	if which < 0 {
+		usage(os.Args[0])
+	}
+
+	for i := 0; i < nworker; i++ {
+		go worker(i, which, os.Args[3])
+	}
+	ninit := 0
+	timeoutChan := time.After(1 * time.Minute)
+	for {
+		select {
+		case <-initChan:
+			ninit += 1
+			if ninit == nworker {
+				goto startBarrier
+			}
+		case <-timeoutChan:
+			fmt.Fprintln(os.Stderr, "Unable to start workers in 1 minute")
+			os.Exit(1)
+		}
+	}
+startBarrier:
+	firstTimeStamp = time.Now()
+	close(startChan)
+
+	ndone := 0
+	timeoutChan = time.After(30 * time.Minute)
+	for {
+		select {
+		case <-doneChan:
+			ndone += 1
+			if ndone == nworker {
+				goto mainDone
+			}
+		case <-timeoutChan:
+			fmt.Fprintln(os.Stderr, "Unable to finish in 30 minutes")
+			os.Exit(1)
+		}
+	}
+
+mainDone:
 	fmt.Println("Done!")
 }
