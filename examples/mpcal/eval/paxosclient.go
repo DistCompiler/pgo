138da29b8a05a2cbb8b2f2425ecd75b679e55805
Renato Costa <renatomc@cs.ubc.ca>
2019-04-24 03:03:20 -0700

    Paxos client: loop if no longer the leader.

--- examples/mpcal/eval/paxosclient.go
+++ examples/mpcal/eval/paxosclient.go
@@ -28,35 +28,48 @@ func NewPaxosClient(addresses []string) (*PaxosClient, error) {
 	}, nil
 }
 
-func (c *PaxosClient) Get(key string) (string, error) {
+func (c *PaxosClient) requestLoop(service string, request map[string]string) (map[string]string, error) {
 	var resp map[string]string
+
+	for {
+		leaderAddress := c.addresses[c.leaderId]
+		if err := c.connections[leaderAddress].Call("KVService.Get", request, &resp); err != nil {
+			return nil, err
+		}
+
+		if resp["type"] == "not_leader_msg" {
+			c.leaderId = (c.leaderId + 1) % len(c.addresses) // start from scratch if needed
+			continue                                         // try again
+		}
+
+		return resp, nil
+	}
+}
+
+func (c *PaxosClient) Get(key string) (string, error) {
 	req := map[string]string{
 		"key": key,
 	}
 
-	leaderAddress := c.addresses[c.leaderId]
-	if err := c.connections[leaderAddress].Call("KVService.Get", req, &resp); err != nil {
+	var resp map[string]string
+	var err error
+
+	resp, err = c.requestLoop("KVService.Get", req)
+	if err != nil {
 		return "", err
 	}
 
-	// TODO: check if leader (resp["type"])
 	return resp["result"], nil
 }
 
 func (c *PaxosClient) Put(key string, value string) error {
-	var resp map[string]string
 	req := map[string]string{
 		"key":   key,
 		"value": value,
 	}
 
-	leaderAddress := c.addresses[c.leaderId]
-	if err := c.connections[leaderAddress].Call("KVService.Put", req, &resp); err != nil {
-		return err
-	}
-
-	// TODO: check if leader (resp["type"])
-	return nil
+	_, err := c.requestLoop("KVService.Put", req)
+	return err
 }
 
 func (c *PaxosClient) Close() {
