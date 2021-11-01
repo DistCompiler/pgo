DistCompiler/pgo

commit 93ebf4d03227570a56bcd5336ebe5296bb67e8f6

fix staticcheck errors

distsys/mpcalctx.go
@@ -23,7 +23,7 @@ var ErrCriticalSectionAborted = errors.New("MPCal critical section aborted")
 var ErrContextClosed = errors.New("MPCal context closed")
 
 // ErrDone exists only to be returned by archetype code implementing the Done label
-var ErrDone = errors.New("A pseudo-error to indicate an archetype has terminated execution normally")
+var ErrDone = errors.New("a pseudo-error to indicate an archetype has terminated execution normally")
 
 // ErrProcedureFallthrough indicated an archetype reached the Error label, and crashed.
 var ErrProcedureFallthrough = errors.New("control has reached the end of a procedure body without reaching a return")
@@ -408,7 +408,7 @@ func (ctx *MPCalContext) ensureArchetypeResource(name string, maker ArchetypeRes
 	handle := ArchetypeResourceHandle(name)
 	// this case accounts for the case where the desired resource already exists
 	if res, ok := ctx.resources[handle]; ok {
-		maker.Configure(res.(ArchetypeResource))
+		maker.Configure(res)
 	} else {
 		res := maker.Make()
 		maker.Configure(res)

distsys/resources/tcpmailboxes.go
@@ -143,6 +143,18 @@ func (res *tcpMailboxesLocal) listen() {
 	}
 }
 
+func (res *tcpMailboxesLocal) setClosing(c bool) {
+	res.lock.Lock()
+	defer res.lock.Unlock()
+	res.closing = c
+}
+
+func (res *tcpMailboxesLocal) getClosing() bool {
+	res.lock.RLock()
+	defer res.lock.RUnlock()
+	return res.closing
+}
+
 func (res *tcpMailboxesLocal) handleConn(conn net.Conn) {
 	defer func() {
 		err := conn.Close()
@@ -187,45 +199,27 @@ func (res *tcpMailboxesLocal) handleConn(conn net.Conn) {
 			if !hasBegun {
 				panic("a correct TCP mailbox exchange must always start with tcpMailboxBegin")
 			}
-			var value tla.TLAValue
-			handle := func() bool {
-				res.lock.RLock()
-				defer res.lock.RUnlock()
-				if res.closing {
-					return true
-				}
-				err = decoder.Decode(&value)
-				if err != nil {
-					return true
-				}
-				localBuffer = append(localBuffer, value)
-				return false
-			}
-			doContinue := handle()
-			if doContinue {
+			if res.getClosing() {
 				continue
 			}
+			var value tla.TLAValue
+			err = decoder.Decode(&value)
+			if err != nil {
+				continue
+			}
+			localBuffer = append(localBuffer, value)
 		case tcpNetworkPreCommit:
 			if !hasBegun {
 				panic("a correct TCP mailbox exchange must always start with tcpMailboxBegin")
 			}
-			handle := func() bool {
-				res.lock.RLock()
-				defer res.lock.RUnlock()
-				if res.closing {
-					return true
-				}
-				err = encoder.Encode(struct{}{})
-				if err != nil {
-					return true
-				}
-				res.wg.Add(1)
-				return false
-			}
-			doContinue := handle()
-			if doContinue {
+			if res.getClosing() {
 				continue
 			}
+			err = encoder.Encode(struct{}{})
+			if err != nil {
+				continue
+			}
+			res.wg.Add(1)
 		case tcpNetworkCommit:
 			if !hasBegun {
 				panic("a correct TCP mailbox exchange must always start with tcpMailboxBegin")
@@ -290,9 +284,7 @@ func (res *tcpMailboxesLocal) WriteValue(value tla.TLAValue) error {
 }
 
 func (res *tcpMailboxesLocal) Close() error {
-	res.lock.Lock()
-	res.closing = true
-	res.lock.Unlock()
+	res.setClosing(true)
 
 	// wait for all the pre-commits that we have responded to be committed
 	res.wg.Wait()

distsys/resources/fd.go
@@ -278,10 +278,11 @@ func (res *singleFailureDetector) ensureClient() error {
 func (res *singleFailureDetector) mainLoop() {
 	res.done = make(chan struct{})
 	res.ticker = time.NewTicker(res.pullInterval)
+loop:
 	for range res.ticker.C {
 		select {
 		case <-res.done:
-			break
+			break loop
 		default:
 		}
 

commit 0219ac3f3f3b867d0fffb36e9e1daecc33a47854

Fix a data race caused by 93ebf4d. Move back err assignment.

distsys/mpcalctx.go
@@ -540,18 +540,17 @@ func (ctx *MPCalContext) Run() error {
 		case nil: // everything is fine; carry on
 		case ErrCriticalSectionAborted:
 			ctx.abort()
+			// we want to keep the invariant that always err is nil after the error
+			// handling in the beginning of the loop. It's easier to read the code and
+			// resaon about it with this invariant.
+			//nolint:ineffassign
+			err = nil
 		case ErrDone: // signals that we're done; quit successfully
 			return nil
 		default:
 			// some other error; return it to caller, we probably crashed
 			return err
 		}
-
-		//nolint:ineffassign
-		// we want to keep the invariant that always err is nil after the error
-		// handling in the beginning of the loop. Easier to read the code and
-		// resaon about it with this invariant.
-		err = nil
 
 		// poll the done channel for Close calls.
 		// this should execute "regularly", since all archetype label implementations are non-blocking

distsys/resources/tcpmailboxes.go
@@ -98,11 +98,14 @@ type tcpMailboxesLocal struct {
 	readBacklog     []tla.TLAValue
 	readsInProgress []tla.TLAValue
 
-	wg   sync.WaitGroup // contains the number of responded pre-commits that we haven't responded to their commits yet.
 	done chan struct{}
 
+	// lock protects closing and synchronizes wg.Add() and wg.Wait(). If
+	// closing is true, then there will be no more wg.Add(). At this point,
+	// using wg.Wait() is safe.
 	lock    sync.RWMutex
 	closing bool
+	wg      sync.WaitGroup // contains the number of responded pre-commits that we haven't responded to their commits yet.
 }
 
 var _ distsys.ArchetypeResource = &tcpMailboxesLocal{}
@@ -141,18 +144,6 @@ func (res *tcpMailboxesLocal) listen() {
 		}
 		go res.handleConn(conn)
 	}
-}
-
-func (res *tcpMailboxesLocal) setClosing(c bool) {
-	res.lock.Lock()
-	defer res.lock.Unlock()
-	res.closing = c
-}
-
-func (res *tcpMailboxesLocal) getClosing() bool {
-	res.lock.RLock()
-	defer res.lock.RUnlock()
-	return res.closing
 }
 
 func (res *tcpMailboxesLocal) handleConn(conn net.Conn) {
@@ -199,27 +190,35 @@ func (res *tcpMailboxesLocal) handleConn(conn net.Conn) {
 			if !hasBegun {
 				panic("a correct TCP mailbox exchange must always start with tcpMailboxBegin")
 			}
-			if res.getClosing() {
-				continue
-			}
 			var value tla.TLAValue
-			err = decoder.Decode(&value)
-			if err != nil {
-				continue
-			}
-			localBuffer = append(localBuffer, value)
+			func() {
+				res.lock.RLock()
+				defer res.lock.RUnlock()
+				if res.closing {
+					return
+				}
+				err = decoder.Decode(&value)
+				if err != nil {
+					return
+				}
+				localBuffer = append(localBuffer, value)
+			}()
 		case tcpNetworkPreCommit:
 			if !hasBegun {
 				panic("a correct TCP mailbox exchange must always start with tcpMailboxBegin")
 			}
-			if res.getClosing() {
-				continue
-			}
-			err = encoder.Encode(struct{}{})
-			if err != nil {
-				continue
-			}
-			res.wg.Add(1)
+			func() {
+				res.lock.RLock()
+				defer res.lock.RUnlock()
+				if res.closing {
+					return
+				}
+				err = encoder.Encode(struct{}{})
+				if err != nil {
+					return
+				}
+				res.wg.Add(1)
+			}()
 		case tcpNetworkCommit:
 			if !hasBegun {
 				panic("a correct TCP mailbox exchange must always start with tcpMailboxBegin")
@@ -284,7 +283,9 @@ func (res *tcpMailboxesLocal) WriteValue(value tla.TLAValue) error {
 }
 
 func (res *tcpMailboxesLocal) Close() error {
-	res.setClosing(true)
+	res.lock.Lock()
+	res.closing = true
+	res.lock.Unlock()
 
 	// wait for all the pre-commits that we have responded to be committed
 	res.wg.Wait()