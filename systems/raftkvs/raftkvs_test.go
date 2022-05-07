1bd0a68f15b2980a6ae694aa65429c5d1d509fb0
Shayan Hosseini <shayan.hosseiny@gmail.com>
2022-05-06 23:45:57 -0700

    add raftkvs liveness tests with multiple clients

--- systems/raftkvs/raftkvs_test.go
+++ systems/raftkvs/raftkvs_test.go
@@ -15,26 +15,28 @@ import (
 	"github.com/dgraph-io/badger/v3"
 )
 
-const monAddr = "localhost:9000"
-const requestTimeout = 200 * time.Millisecond
+const (
+	monAddr        = "localhost:9000"
+	requestTimeout = 200 * time.Millisecond
 
-const fdPullInterval = 200 * time.Millisecond
-const fdTimeout = 100 * time.Millisecond
+	fdPullInterval = 200 * time.Millisecond
+	fdTimeout      = 100 * time.Millisecond
 
-const mailboxesDialTimeout = 50 * time.Millisecond
-const mailboxesReadTimeout = 50 * time.Millisecond
-const mailboxesWriteTimeout = 50 * time.Millisecond
+	mailboxesDialTimeout  = 50 * time.Millisecond
+	mailboxesReadTimeout  = 50 * time.Millisecond
+	mailboxesWriteTimeout = 50 * time.Millisecond
 
-const leaderElectionTimeout = 150 * time.Millisecond
-const leaderElectionTimeoutOffset = 150 * time.Millisecond
+	leaderElectionTimeout       = 150 * time.Millisecond
+	leaderElectionTimeoutOffset = 150 * time.Millisecond
 
-const appendEntriesSendInterval = 10 * time.Millisecond
+	appendEntriesSendInterval = 10 * time.Millisecond
 
-const sharedResourceTimeout = 2 * time.Millisecond
+	sharedResourceTimeout = 2 * time.Millisecond
 
-const inputChannelReadTimeout = 2 * time.Millisecond
+	inputChannelReadTimeout = 2 * time.Millisecond
 
-const valueSize = 10
+	valueSize = 10
+)
 
 type mailboxesMaker func(fn resources.MailboxesAddressMappingFn, opts ...resources.MailboxesOption) *resources.Mailboxes
 
@@ -56,7 +58,8 @@ func newNetwork(self tla.TLAValue, maker mailboxesMaker) *resources.Mailboxes {
 	)
 }
 
-func newServerCtxs(srvId tla.TLAValue, constants []distsys.MPCalContextConfigFn, mboxMaker mailboxesMaker, db *badger.DB) []*distsys.MPCalContext {
+func newServerCtxs(srvId tla.TLAValue, constants []distsys.MPCalContextConfigFn,
+	mboxMaker mailboxesMaker, db *badger.DB) []*distsys.MPCalContext {
 	iface := distsys.NewMPCalContextWithoutArchetype(constants...).IFace()
 
 	toMap := func(res distsys.ArchetypeResource) distsys.ArchetypeResource {
@@ -274,6 +277,33 @@ func setupMonitor() *resources.Monitor {
 	return mon
 }
 
+func checkResp(t *testing.T, resp tla.TLAValue, j int, reqs []tla.TLAValue,
+	iface distsys.ArchetypeInterface) {
+	if resp.ApplyFunction(tla.MakeTLAString("msuccess")).Equal(tla.TLA_FALSE) {
+		t.Fatal("got an unsuccessful response")
+	}
+
+	typ := resp.ApplyFunction(tla.MakeTLAString("mtype"))
+	body := resp.ApplyFunction(tla.MakeTLAString("mresponse"))
+	key := body.ApplyFunction(tla.MakeTLAString("key"))
+	value := body.ApplyFunction(tla.MakeTLAString("value"))
+	reqKey := reqs[j].ApplyFunction(tla.MakeTLAString("key"))
+	if !key.Equal(reqKey) {
+		t.Fatalf("wrong response key, expected: %v, got: %v", reqKey, key)
+	}
+	if typ.Equal(raftkvs.ClientGetResponse(iface)) {
+		reqValue := reqs[j-1].ApplyFunction(tla.MakeTLAString("value"))
+		if !value.Equal(reqValue) {
+			t.Fatalf("wrong response value, expected: %v, got: %v", reqValue, value)
+		}
+	} else if typ.Equal(raftkvs.ClientPutRequest(iface)) {
+		reqValue := reqs[j].ApplyFunction(tla.MakeTLAString("value"))
+		if !value.Equal(reqValue) {
+			t.Fatalf("wrong response value, expected: %v, got: %v", reqValue, value)
+		}
+	}
+}
+
 func runSafetyTest(t *testing.T, numServers int, netMaker mailboxesMaker, numNodeFail int) {
 	numClients := 1
 	numRequestPairs := 10
@@ -315,7 +345,7 @@ func runSafetyTest(t *testing.T, numServers int, netMaker mailboxesMaker, numNod
 			ctx := serverCtxs[i]
 			go func() {
 				err := mon.RunArchetype(ctx)
-				log.Printf("archetype = %v, err = %v", ctx.IFace().Self(), err)
+				log.Printf("archetype %v finished, err = %v", ctx.IFace().Self(), err)
 				errs <- err
 			}()
 		}
@@ -329,7 +359,7 @@ func runSafetyTest(t *testing.T, numServers int, netMaker mailboxesMaker, numNod
 		ctxs = append(ctxs, clientCtx)
 		go func() {
 			err := clientCtx.Run()
-			log.Printf("archetype = %v, err = %v", clientCtx.IFace().Self(), err)
+			log.Printf("archetype %v finished, err = %v", clientCtx.IFace().Self(), err)
 			errs <- err
 		}()
 	}
@@ -350,8 +380,9 @@ func runSafetyTest(t *testing.T, numServers int, netMaker mailboxesMaker, numNod
 		}
 	}()
 
-	log.Println("waiting 3 second")
-	time.Sleep(3 * time.Second)
+	delay := 3 * time.Second
+	log.Printf("waiting %v", delay)
+	time.Sleep(delay)
 
 	if numNodeFail > 0 {
 		go func() {
@@ -392,7 +423,6 @@ func runSafetyTest(t *testing.T, numServers int, netMaker mailboxesMaker, numNod
 
 	j := 0
 	for j < numRequests {
-		// log.Printf("waiting for response %d", j+1)
 		if len(timeoutCh) != 0 {
 			t.Fatal("timeoutCh is not empty")
 		}
@@ -412,31 +442,128 @@ func runSafetyTest(t *testing.T, numServers int, netMaker mailboxesMaker, numNod
 
 		log.Printf("response %d: %v", j+1, resp)
 
-		if resp.ApplyFunction(tla.MakeTLAString("msuccess")).Equal(tla.TLA_FALSE) {
-			t.Fatal("got an unsuccessful response")
-		}
-
-		typ := resp.ApplyFunction(tla.MakeTLAString("mtype"))
-		body := resp.ApplyFunction(tla.MakeTLAString("mresponse"))
-		key := body.ApplyFunction(tla.MakeTLAString("key"))
-		value := body.ApplyFunction(tla.MakeTLAString("value"))
-		reqKey := reqs[j].ApplyFunction(tla.MakeTLAString("key"))
-		if !key.Equal(reqKey) {
-			t.Fatalf("wrong response key, expected: %v, got: %v", reqKey, key)
-		}
-		if typ.Equal(raftkvs.ClientGetResponse(iface)) {
-			reqValue := reqs[j-1].ApplyFunction(tla.MakeTLAString("value"))
-			if !value.Equal(reqValue) {
-				t.Fatalf("wrong response value, expected: %v, got: %v", reqValue, value)
-			}
-		} else if typ.Equal(raftkvs.ClientPutRequest(iface)) {
-			reqValue := reqs[j].ApplyFunction(tla.MakeTLAString("value"))
-			if !value.Equal(reqValue) {
-				t.Fatalf("wrong response value, expected: %v, got: %v", reqValue, value)
-			}
-		}
+		checkResp(t, resp, j, reqs, iface)
 		j += 1
 	}
+}
+
+func createAndRunClient(self tla.TLAValue, numRequests int,
+	constants []distsys.MPCalContextConfigFn, netMaker mailboxesMaker) error {
+	iface := distsys.NewMPCalContextWithoutArchetype().IFace()
+
+	reqCh := make(chan tla.TLAValue, numRequests)
+	respCh := make(chan tla.TLAValue, numRequests)
+	timeoutCh := make(chan tla.TLAValue)
+	ctx := newClientCtx(self, constants, reqCh, respCh, timeoutCh, netMaker)
+
+	errCh := make(chan error)
+	go func() {
+		err := ctx.Run()
+		log.Printf("archetype %v finished, err = %v", ctx.IFace().Self(), err)
+		errCh <- err
+	}()
+
+	for i := 0; i < numRequests; i++ {
+		key := tla.MakeTLAString("key")
+		valueStr := fmt.Sprintf("value%d", i)
+		value := tla.MakeTLAString(valueStr)
+		putReq := tla.MakeTLARecord([]tla.TLARecordField{
+			{Key: tla.MakeTLAString("type"), Value: raftkvs.Put(iface)},
+			{Key: tla.MakeTLAString("key"), Value: key},
+			{Key: tla.MakeTLAString("value"), Value: value},
+		})
+		reqCh <- putReq
+
+	forLoop:
+		for {
+			select {
+			case <-respCh:
+				break forLoop
+			case <-time.After(requestTimeout):
+				log.Printf("sending timeout for response %d", i+1)
+				select {
+				case timeoutCh <- tla.TLA_TRUE:
+					log.Printf("sent timeout for response %d", i+1)
+				case <-time.After(requestTimeout):
+				}
+			}
+		}
+	}
+
+	ctx.Stop()
+	return <-errCh
+}
+
+func runLivenessTest(t *testing.T, numServers, numClients int, netMaker mailboxesMaker) {
+	clientNumRequests := 100
+
+	constants := append([]distsys.MPCalContextConfigFn{
+		distsys.DefineConstantValue("NumServers", tla.MakeTLANumber(int32(numServers))),
+		distsys.DefineConstantValue("NumClients", tla.MakeTLANumber(int32(numClients))),
+		distsys.DefineConstantValue("ExploreFail", tla.TLA_FALSE),
+		distsys.DefineConstantValue("Debug", tla.TLA_FALSE),
+	}, raftkvs.PersistentLogConstantDefs, raftkvs.LeaderTimeoutConstantDefs)
+	mon := setupMonitor()
+
+	db, err := badger.Open(badger.DefaultOptions("/tmp/badger"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	var ctxs []*distsys.MPCalContext
+
+	serverErrs := make(chan error)
+	for i := 1; i <= numServers; i++ {
+		serverCtxs := newServerCtxs(tla.MakeTLANumber(int32(i)), constants, netMaker, db)
+		ctxs = append(ctxs, serverCtxs...)
+		for i := range serverCtxs {
+			ctx := serverCtxs[i]
+			go func() {
+				err := mon.RunArchetype(ctx)
+				log.Printf("archetype %v finished, err = %v", ctx.IFace().Self(), err)
+				serverErrs <- err
+			}()
+		}
+	}
+
+	delay := 3 * time.Second
+	log.Printf("waiting %v", delay)
+	time.Sleep(delay)
+
+	log.Println("starting clients")
+	clientErrs := make(chan error)
+	for i := 6*numServers + 1; i <= 6*numServers+numClients; i++ {
+		self := tla.MakeTLANumber(int32(i))
+		go func() {
+			clientErrs <- createAndRunClient(self, clientNumRequests, constants, netMaker)
+		}()
+	}
+
+	for i := 0; i < numClients; i++ {
+		err := <-clientErrs
+		log.Printf("client %d error: %v", i, err)
+	}
+
+	defer func() {
+		for _, ctx := range ctxs {
+			log.Printf("stopping archetype: %v", ctx.IFace().Self())
+			ctx.Stop()
+		}
+		for i := 0; i < len(ctxs); i++ {
+			err := <-serverErrs
+			if err != nil {
+				t.Errorf("archetype error: %v", err)
+			}
+		}
+		if err := mon.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
 }
 
 func TestSafety_ThreeServers(t *testing.T) {
@@ -447,9 +574,10 @@ func TestSafety_FiveServers(t *testing.T) {
 	runSafetyTest(t, 5, resources.NewRelaxedMailboxes, 0)
 }
 
-func TestSafety_SevenServers(t *testing.T) {
-	runSafetyTest(t, 7, resources.NewRelaxedMailboxes, 0)
-}
+// Commented out because it requires a lot of resources.
+// func TestSafety_SevenServers(t *testing.T) {
+// 	runSafetyTest(t, 7, resources.NewRelaxedMailboxes, 0)
+// }
 
 func TestSafety_OneFailling_ThreeServers(t *testing.T) {
 	runSafetyTest(t, 3, resources.NewRelaxedMailboxes, 1)
@@ -461,4 +589,12 @@ func TestSafety_OneFailling_FiveServers(t *testing.T) {
 
 func TestSafety_TwoFailling_FiveServers(t *testing.T) {
 	runSafetyTest(t, 5, resources.NewRelaxedMailboxes, 2)
+}
+
+func TestLiveness_ThreeServers_ThreeClients(t *testing.T) {
+	runLivenessTest(t, 3, 3, resources.NewRelaxedMailboxes)
+}
+
+func TestLiveness_ThreeServers_FiveClients(t *testing.T) {
+	runLivenessTest(t, 3, 5, resources.NewRelaxedMailboxes)
 }
