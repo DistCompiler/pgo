2045796baac733902f771a9a49aab43af8e66fcf
Shayan Hosseini <shayan.hosseiny@gmail.com>
2022-05-17 14:38:48 -0700

    added profiling for the raftkvs server

--- systems/raftkvs/cmd/server/main.go
+++ systems/raftkvs/cmd/server/main.go
@@ -4,25 +4,52 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
 
 	"github.com/DistCompiler/pgo/systems/raftkvs/bootstrap"
 	"github.com/DistCompiler/pgo/systems/raftkvs/configs"
 	"github.com/dgraph-io/badger/v3"
+	"github.com/pkg/profile"
 )
 
 func main() {
-	var srvIdInt int
+	var srvId int
 	var configPath string
-	flag.IntVar(&srvIdInt, "srvId", -1, "Server ID")
+	var profileMode string
+	flag.IntVar(&srvId, "srvId", -1, "Server ID")
 	flag.StringVar(&configPath, "c", "", "Config file")
+	flag.StringVar(&profileMode, "profile.mode", "", "enable profiling mode, one of [cpu, mem, mutex, block, goroutine, trace]")
 
 	flag.Parse()
 
-	if srvIdInt == -1 {
+	if srvId == -1 {
 		log.Fatal("srvId is not provided or it is invalid")
 	}
 	if configPath == "" {
 		log.Fatal("config file is not provided")
+	}
+	if profileMode != "" {
+		profilePath := filepath.Join("profiles", fmt.Sprintf("srv%d", srvId))
+
+		switch profileMode {
+		case "cpu":
+			defer profile.Start(profile.CPUProfile, profile.ProfilePath(profilePath)).Stop()
+		case "mem":
+			defer profile.Start(profile.MemProfile, profile.ProfilePath(profilePath)).Stop()
+		case "mutex":
+			defer profile.Start(profile.MutexProfile, profile.ProfilePath(profilePath)).Stop()
+		case "block":
+			defer profile.Start(profile.BlockProfile, profile.ProfilePath(profilePath)).Stop()
+		case "goroutine":
+			defer profile.Start(profile.GoroutineProfile, profile.ProfilePath(profilePath)).Stop()
+		case "trace":
+			defer profile.Start(profile.TraceProfile, profile.ProfilePath(profilePath)).Stop()
+		default:
+			log.Fatal("invalid profile.mode value")
+		}
 	}
 
 	c, err := configs.ReadConfig(configPath)
@@ -30,7 +57,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	dbPath := fmt.Sprintf("/tmp/server%d/badger", srvIdInt)
+	dbPath := fmt.Sprintf("/tmp/server%d/badger", srvId)
 	db, err := badger.Open(badger.DefaultOptions(dbPath))
 	if err != nil {
 		log.Fatal(err)
@@ -41,7 +68,7 @@ func main() {
 		}
 	}()
 
-	s := bootstrap.NewServer(srvIdInt, c, db)
+	s := bootstrap.NewServer(srvId, c, db)
 
 	go func() {
 		err := s.Run()
@@ -57,5 +84,10 @@ func main() {
 		}
 	}()
 
-	select {}
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	select {
+	case <-sigCh:
+		log.Println("received SIGTERM")
+	}
 }
--- systems/raftkvs/go.mod
+++ systems/raftkvs/go.mod
@@ -32,6 +32,7 @@ require (
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/profile v1.6.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
