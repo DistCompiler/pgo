9453fed15759a9f2f20bfb7a67750a69a119c1f0
Shayan Hosseini <shayan.hosseiny@gmail.com>
2022-06-27 22:58:26 -0700

    remove conflict resolution from hashmap to make it simpler and faster

--- distsys/hashmap/hashmap.go
+++ distsys/hashmap/hashmap.go
@@ -4,53 +4,25 @@ import (
 	"github.com/UBC-NSS/pgo/distsys/tla"
 )
 
-type Entry[V any] struct {
-	Key   tla.TLAValue
-	Value V
-}
-
 type HashMap[V any] struct {
-	M    map[uint32][]Entry[V]
+	M    map[uint32]V
 	Keys []tla.TLAValue
 }
 
 func New[V any]() *HashMap[V] {
-	return &HashMap[V]{M: make(map[uint32][]Entry[V])}
+	return &HashMap[V]{M: make(map[uint32]V)}
 }
 
 func (h *HashMap[V]) Set(k tla.TLAValue, v V) {
-	entry := Entry[V]{
-		Key:   k,
-		Value: v,
-	}
-	hash := k.Hash()
-	if _, ok := h.M[hash]; !ok {
+	if _, ok := h.Get(k); !ok {
 		h.Keys = append(h.Keys, k)
-		h.M[hash] = append(h.M[hash], entry)
-	} else {
-		for i := range h.M[hash] {
-			if h.M[hash][i].Key.Equal(k) {
-				h.M[hash][i].Value = v
-				return
-			}
-		}
-		h.Keys = append(h.Keys, k)
-		h.M[hash] = append(h.M[hash], entry)
 	}
+	h.M[k.Hash()] = v
 }
 
-func (h *HashMap[V]) Get(k tla.TLAValue) (V, bool) {
-	var v V
-	entries, ok := h.M[k.Hash()]
-	if !ok {
-		return v, false
-	}
-	for _, e := range entries {
-		if e.Key.Equal(k) {
-			return e.Value, true
-		}
-	}
-	return v, false
+func (h *HashMap[V]) Get(k tla.TLAValue) (v V, ok bool) {
+	v, ok = h.M[k.Hash()]
+	return
 }
 
 func (h *HashMap[V]) Clear() {
