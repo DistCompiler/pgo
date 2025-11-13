6829715d66b2c26861df9779baa4c48dfc29104a
Finn Hackett <fhackett.py@gmail.com>
2025-11-13 05:36:26 +0000

    improve plots + parity check

--- omnilink/plots/setbench_porcupine_mem_plt.py
+++ omnilink/plots/setbench_porcupine_mem_plt.py
@@ -8,19 +8,19 @@ data = json.loads(sys.stdin.read())
 
 fig, ax = plt.subplots()
 
-def plot_pair(pair, *, label):
+def plot_pair(pair, *, label, marker):
     [counts, durations] = pair
-    ax.scatter(counts, durations, label=label)
+    ax.scatter(counts, durations, label=label, marker=marker)
 
-plot_pair(data['omnilink_success'], label='OmniLink (valid)')
-plot_pair(data['omnilink_failure'], label='OmniLink (invalid)')
-plot_pair(data['porcupine_success'], label='Porcupine (valid)')
-plot_pair(data['porcupine_failure'], label='Porcupine (invalid)')
+plot_pair(data['omnilink_success'], label='OmniLink (valid)', marker='1')
+plot_pair(data['omnilink_failure'], label='OmniLink (invalid)', marker='2')
+plot_pair(data['porcupine_success'], label='Porcupine (valid)', marker='3')
+plot_pair(data['porcupine_failure'], label='Porcupine (invalid)', marker='4')
 
 ax.set_xlabel("Operation count")
 ax.set_xticks(data['ticks'], labels=data['tick_labels'])
 ax.set_ylabel('Peak memory usage (GB)')
 
-fig.legend(loc='upper center')
+fig.legend(loc='upper center', ncols=2)
 
 plt.savefig('mem_plot.pdf')
--- omnilink/plots/setbench_porcupine_time_plt.py
+++ omnilink/plots/setbench_porcupine_time_plt.py
@@ -7,19 +7,20 @@ data = json.loads(sys.stdin.read())
 
 fig, ax = plt.subplots()
 
-def plot_pair(pair, *, label):
+def plot_pair(pair, *, label, marker):
     [counts, durations] = pair
-    ax.scatter(counts, durations, label=label)
+    ax.scatter(counts, durations, label=label, marker=marker)
 
-plot_pair(data['omnilink_success'], label='OmniLink (valid)')
-plot_pair(data['omnilink_failure'], label='OmniLink (invalid)')
-plot_pair(data['porcupine_success'], label='Porcupine (valid)')
-plot_pair(data['porcupine_failure'], label='Porcupine (invalid)')
+plot_pair(data['omnilink_success'], label='OmniLink (valid)', marker='1')
+plot_pair(data['omnilink_failure'], label='OmniLink (invalid)', marker='2')
+plot_pair(data['porcupine_success'], label='Porcupine (valid)', marker='3')
+plot_pair(data['porcupine_failure'], label='Porcupine (invalid)', marker='4')
 
 ax.set_xlabel("Operation count")
+# ax.set_yscale('log', base=60)
 ax.set_xticks(data['ticks'], labels=data['tick_labels'])
 ax.set_ylabel('Runtime (s)')
 
-fig.legend(loc='upper center')
+fig.legend(loc='upper center', ncols=2)
 
 plt.savefig('time_plot.pdf')
